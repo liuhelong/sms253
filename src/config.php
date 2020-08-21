d15d47ce50578e488885c9e4083a5d0d9e3c7baf
liu <854583481@qq.com>
2020-08-21 10:46:35 +0800

    Create config.php

---
+++ src/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the liuhelong/253sms.
+ *
+ * 
+ */
+
+return [
+
+    'api' => env('253API','api'),
+	'account' => env('253ACCOUNT','account'),
+	'password' => env('253PASSWORD','password'),
+];
